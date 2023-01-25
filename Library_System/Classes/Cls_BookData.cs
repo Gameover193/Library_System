e9860292180fb20dbc75f8772c211b042ab1f552
Elsedik <Elsedik@gmail.com>
2023-01-25 13:38:22 +0200

    add Country

--- Library_System/Classes/Cls_BookData.cs
+++ Library_System/Classes/Cls_BookData.cs
@@ -8,5 +8,10 @@ namespace Library_System.Classes
 {
     class Cls_BookData
     {
+        public int Book_ID { get; set; }
+        public string Book_Name { get; set; }
+        public string Book_Author { get; set; }
+        public int Book_PageNumber { get; set; }
+        public string Book_Country { get; set; }
     }
 }
